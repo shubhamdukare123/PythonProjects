5d7cfc39357b617a2644c678b904050b1efada86
shubhamdukare123 <shubhamsdukare@gmail.com>
2024-02-19 19:34:21 +0530

    updated

--- tutorials/main.py
+++ tutorials/main.py
@@ -2,12 +2,15 @@ from tkinter import *
 
 root = Tk()
 
-label = Label(root, text = "Hello World")
-label.pack()
+label1 = Label(root, text = "Hello World 1 ")
+label2 = Label(root, text = "Hello World 2 ")
+label1.grid(row=0, column = 0)
+label2.grid(row=1, column = 1)
+
 
 
 
 
 root.title("Posts page")
-root.geometry("1920x1080")
+#root.geometry("1920x1080")
 root.mainloop()
